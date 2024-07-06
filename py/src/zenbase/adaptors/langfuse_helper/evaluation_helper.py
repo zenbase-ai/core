70b23abfd4a46a361dd9db0c6565fb72506ab2ed
Amir Mehr <amir.saiedmehr@gmail.com>
2024-07-06 13:58:59 -0600

    add: langfuse helpers.

--- py/src/zenbase/adaptors/langfuse_helper/evaluation_helper.py
+++ py/src/zenbase/adaptors/langfuse_helper/evaluation_helper.py
@@ -3,16 +3,18 @@ from typing import Callable
 from langfuse import Langfuse
 from langfuse.client import Dataset
 
+from zenbase.adaptors.base.evaluation_helper import BaseEvaluationHelper
 from zenbase.optim.metric.types import (
     CandidateEvalResult,
     CandidateEvaluator,
+    IndividualEvalValue,
     OverallEvalValue,
 )
 from zenbase.types import LMDemo, LMFunction, Outputs
 from zenbase.utils import pmap
 
 
-class ZenLangfuse:
+class LangfuseEvaluationHelper(BaseEvaluationHelper):
     MetricEvaluator = Callable[[list[OverallEvalValue]], OverallEvalValue]
 
     @staticmethod
@@ -25,7 +27,52 @@ class ZenLangfuse:
 
     @staticmethod
     def dataset_demos(dataset: Dataset) -> list[LMDemo]:
+        # TODO: Should remove and deprecate
         return [LMDemo(inputs=item.input, outputs=item.expected_output) for item in dataset.items]
+
+    @staticmethod
+    def _metric_evaluator_generator(
+        data: list[LMDemo],
+        evaluate: Callable[[Outputs, LMDemo, Langfuse], OverallEvalValue],
+        candidate_evals: MetricEvaluator = default_candidate_evals,
+        langfuse: Langfuse | None = None,
+        concurrency: int = 20,
+        threshold: float = 0.5,
+    ) -> CandidateEvaluator:
+        from langfuse import Langfuse
+        from langfuse.decorators import observe
+
+        langfuse = langfuse or Langfuse()
+
+        def evaluate_candidate(function: LMFunction) -> CandidateEvalResult:
+            individual_evals = []
+
+            @observe()
+            def run_and_evaluate(demo: LMDemo):
+                nonlocal individual_evals
+
+                outputs = function(demo.inputs)
+                evals = evaluate(outputs, demo, langfuse=langfuse)
+                individual_evals.append(
+                    IndividualEvalValue(
+                        passed=evals["score"] >= threshold,
+                        response=outputs,
+                        demo=demo,
+                        score=evals["score"],
+                    )
+                )
+                return evals
+
+            item_evals = pmap(
+                run_and_evaluate,
+                data,
+                concurrency=concurrency,
+            )
+            candidate_eval = candidate_evals(item_evals)
+
+            return CandidateEvalResult(function, candidate_eval, individual_evals=individual_evals)
+
+        return evaluate_candidate
 
     @classmethod
     def metric_evaluator(
@@ -35,7 +82,9 @@ class ZenLangfuse:
         candidate_evals: MetricEvaluator = default_candidate_evals,
         langfuse: Langfuse | None = None,
         concurrency: int = 20,
+        threshold: float = 0.5,
     ) -> CandidateEvaluator:
+        # TODO: Should remove and deprecate
         from langfuse import Langfuse
         from langfuse.decorators import observe
 
@@ -58,3 +107,6 @@ class ZenLangfuse:
             return CandidateEvalResult(function, candidate_eval)
 
         return evaluate_candidate
+
+    def get_evaluator(self, data: str):
+        raise NotImplementedError("This method should be implemented by the parent class as it needs access to data")
