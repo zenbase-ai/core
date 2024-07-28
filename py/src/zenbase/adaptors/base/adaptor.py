692b5bf8e633946f81b6af34dc4e78a9281556be
Amir Mehr <amir.saiedmehr@gmail.com>
2024-07-28 16:05:16 -0600

    Refactor initialization logic in ZenAdaptor constructor.

--- py/src/zenbase/adaptors/base/adaptor.py
+++ py/src/zenbase/adaptors/base/adaptor.py
@@ -1,8 +1,6 @@
 from abc import ABC
 
-from langsmith import Client
-
 
 class ZenAdaptor(ABC):
     def __init__(self, client=None):
-        self.client = client if client else Client()
+        self.client = client
